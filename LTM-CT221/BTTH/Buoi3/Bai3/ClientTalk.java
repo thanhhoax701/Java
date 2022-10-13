a0980b40cb44c4fe5c1b45e876ed58ca6e898e15
thanhhoax701 <tth1610.vl@gmail.com>
2022-10-13 16:42:26 +0700

    xong bài 3 - buổi 3

--- LTM-CT221/BTTH/Buoi3/Bai3/ClientTalk.java
+++ LTM-CT221/BTTH/Buoi3/Bai3/ClientTalk.java
@@ -3,5 +3,32 @@ import java.net.*;
 import java.util.Scanner;
 
 public class ClientTalk {
-    
+    public static final int port = 1410;
+
+    public static void main(String[] args) {
+        try {
+            // Tao UDP Socket
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                // Gui du lieu
+                System.out.println("Tao noi: ");
+                String ouputStr = sc.nextLine();
+                DatagramPacket outputPack = new DatagramPacket(ouputStr.getBytes(), ouputStr.length(), serverAddress,
+                        port);
+                socket.send(outputPack);
+
+                // Nhan du lieu
+                byte intputByte[] = new byte[6000000];
+                DatagramPacket inputPack = new DatagramPacket(intputByte, intputByte.length);
+                socket.receive(inputPack);
+
+                String inputStr = new String(inputPack.getData(), 0, inputPack.getLength());
+                System.out.println("May noi: " + inputStr);
+            }
+        } catch (IOException e) {
+            System.out.println("Loi Client: " + e.toString());
+        }
+    }
 }
--- LTM-CT221/BTTH/Buoi3/Bai3/ServerTalk.java
+++ LTM-CT221/BTTH/Buoi3/Bai3/ServerTalk.java
@@ -1,6 +1,31 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class ServerTalk {
-    
+    public static final int port = 1410;
+
+    public static void main(String[] args) {
+        try{
+            DatagramSocket socket = new DatagramSocket(port);
+            Scanner sc = new Scanner(System.in);
+            byte inputByte[] = new byte[6000000];
+            while(true) {
+                // Nhan ten file tu Client
+                DatagramPacket inputPack = new DatagramPacket(inputByte, inputByte.length);
+                socket.receive(inputPack);
+                String inputStr = new String(inputPack.getData(), 0, inputPack.getLength());
+                System.out.println("May noi: " + inputStr);
+
+                // Gui du lieu cho Client
+                System.out.println("Tao noi: ");
+                String outputStr = sc.nextLine();
+                DatagramPacket outputPack = new DatagramPacket(outputStr.getBytes(), outputStr.length()
+                , inputPack.getAddress(), inputPack.getPort());
+                socket.send(outputPack);
+            }
+        } catch(IOException e) {
+            System.out.println("Loi Server: " + e.toString());
+        }
+    }
 }
